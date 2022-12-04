74e728765e59f3390ed5987e15ee8e44e5d58fee
Nora <norapasquale@gmail.com>
2022-12-04 15:06:56 +0100

    prove titolo

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,26 +262,3 @@
 
 
 
-/** NOstro**/
-
-function showCheckboxes() {
-  var checkboxes = document.getElementById("checkboxes");
-  if (expanded) {
-    checkboxes.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes.style.display = "none";
-    expanded = false;
-  }
-}
-
-function showCheckboxes2() {
-  var checkboxes2 = document.getElementById("checkboxes2");
-  if (expanded) {
-    checkboxes2.style.display = "block";
-    expanded = true;
-  } else {
-    checkboxes2.style.display = "none";
-    expanded = false;
-  }
-}
