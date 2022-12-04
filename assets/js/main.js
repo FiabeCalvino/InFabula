f3a0e7d68292c297d10883f87d3d1f985eaacc1a
Nora <norapasquale@gmail.com>
2022-12-04 15:08:00 +0100

    Revert "prove titolo"
    This reverts commit 74e728765e59f3390ed5987e15ee8e44e5d58fee.

--- assets/js/main.js
+++ assets/js/main.js
@@ -262,3 +262,26 @@
 
 
 
+/** NOstro**/
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes2() {
+  var checkboxes2 = document.getElementById("checkboxes2");
+  if (expanded) {
+    checkboxes2.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes2.style.display = "none";
+    expanded = false;
+  }
+}
