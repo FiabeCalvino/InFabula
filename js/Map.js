534df5922dd1eea69c7f701f52e4e789fa604427
Nora <norapasquale@gmail.com>
2022-12-06 01:36:30 +0100

    leaflet e co

---
+++ js/Map.js
@@ -0,0 +1,10 @@
+const map = L.map('map').setView([41.384, 13.118], 6);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	const marker = L.marker([51.5, -0.09]).addTo(map);
+
+	
